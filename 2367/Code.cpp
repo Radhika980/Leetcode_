99380510dcefc56e36576639975cbf12c2c6a2ef
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-03-22 11:11:46 +0530

    Update Code.cpp

--- 2367/Code.cpp
+++ 2367/Code.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    int arithmeticTriplets(vector<int>& nums, int diff)
+    {
+       int c=0;
+       for(int i=0;i<nums.size();i++)
+       {
+        int no1=nums[i]+diff;
+        if(count(nums.begin(),nums.end(),no1)>0)
+        {
+          int no2=no1+diff;
+           if(count(nums.begin(),nums.end(),no2)>0)
+            {
+              c++;
+            }
+        }
+       }
+       return c;
+    }
+};
