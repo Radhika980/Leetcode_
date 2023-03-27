49a91cab5d70c6fe2bd1a767310474a4df8e0413
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-03-27 11:10:41 +0530

    Update code.cpp

--- 1015/code.cpp
+++ 1015/code.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int smallestRepunitDivByK(int k) 
+    {
+        int j=0;
+        int ct=1, i=0;
+        while(j!=k)
+        {
+            i=(10*i+1)%k;
+            if(i==0)return ct;
+            else ct++;
+            j++;
+        }
+        
+        return -1;
+        
+    }
+};
