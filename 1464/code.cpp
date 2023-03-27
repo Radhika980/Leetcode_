1aa5afd5a47243602aca25c9a4c7b388fd5c367c
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-03-27 15:01:49 +0530

    Update code.cpp

--- 1464/code.cpp
+++ 1464/code.cpp
@@ -1,1 +1,13 @@
 
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) 
+    {
+        int t=1;
+        sort(nums.begin(),nums.end(),greater<int>());
+       
+            t=(nums[0]-1)*(nums[1]-1);
+        
+        return t;
+    }
+};
