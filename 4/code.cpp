9c69abb3a152ed7db596974d8516c670da2a3f93
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-03-29 10:55:41 +0530

    Update code.cpp

--- 4/code.cpp
+++ 4/code.cpp
@@ -1,1 +1,32 @@
-
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
+    {
+      
+        double med;
+        for(int i=0;i<nums2.size();i++)
+        {
+            nums1.push_back(nums2[i]);
+        }
+        
+       
+        int n=nums1.size();
+        sort(nums1.begin(),nums1.end());
+       
+        int m,l;
+        
+            if(n%2==0)
+            {
+                m=n/2;
+                l=m-1;
+                med=(nums1[m]+nums1[l])/(double)2;
+            }
+            else
+            {
+                m=n/2;
+                med=nums1[m];
+            }
+        
+        return med;
+    }
+};
