f6fb981c1db55c2e65709b4ca6b6e12283bf73ef
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-03-30 18:48:06 +0530

    Update code.cpp

--- 344/code.cpp
+++ 344/code.cpp
@@ -1,1 +1,21 @@
 
+class Solution {
+public:
+    void reverseString(vector<char>& s) 
+    {
+         int i=0;
+         int j=s.size()-1;
+          while(i<j)
+         {
+          swap(s[i],s[j]);
+          i++;
+          j--;
+
+         }
+          for(int k=0;k<s.size();k++)
+         {
+           cout<<s[k];
+         }
+        
+    }
+};
