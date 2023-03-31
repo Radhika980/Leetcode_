afa5de12e81a4ec679595b856e30d04fd7d3502b
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-03-31 12:10:18 +0530

    Update code.cpp

--- 33/code.cpp
+++ 33/code.cpp
@@ -1,1 +1,18 @@
 
+class Solution {
+public:
+    int search(vector<int>& nums, int target)
+    {
+        int m,index=-1;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(target==nums[i])
+            {
+                
+                index=i;
+            }
+        }
+        return index;
+        
+    }
+};
