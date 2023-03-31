7370e81c754853693921adf28dde563ce7b238f3
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-03-31 12:20:25 +0530

    Update code.cpp

--- 154/code.cpp
+++ 154/code.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int findMin(vector<int>& nums) 
+    {
+        
+        int m,index=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(m>nums[i])
+            {
+                m=nums[i];
+                index=i;
+            }
+        }
+        return m;
+        
+    }
+};
