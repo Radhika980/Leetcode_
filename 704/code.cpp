48ef4cbe3ed626573b391f280adf12b8f014e77a
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-01 13:31:26 +0530

    Update code.cpp

--- 704/code.cpp
+++ 704/code.cpp
@@ -1,1 +1,20 @@
 
+class Solution {
+public:
+    int search(vector<int>& nums, int target) 
+    {
+        int s=0;
+        int e=nums.size();
+        int mid=(s+e)/2;
+        if(nums.size()==1 && nums[0]!=target)return -1;
+        while(s<=e)
+        {
+            mid=(s+e)/2;
+            if(nums[mid]==target)return mid;
+            else if(nums[mid]<target)s=mid+1;
+            else if(nums[mid]>target)e=mid-1;
+        }
+        return -1;
+        
+    }
+};
