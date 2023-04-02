3a1c0099c39c8bef18f0f77a0b7117feb56063bd
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-02 13:15:50 +0530

    Update code.cpp

--- 34/code.cpp
+++ 34/code.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target)
+    {
+        vector<int>ans{-1,-1};
+        vector<int>pp;
+        int k,l;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==target)
+            {
+               k=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+                l=upper_bound(nums.begin(),nums.end(),target)-nums.begin();
+                pp.push_back(k);
+                pp.push_back(l-1);
+                return pp;
+            }
+        }
+        return ans;
+    }
+};
