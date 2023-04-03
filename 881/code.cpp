1ecf217311d4dbce9c8195d195fe7cca61ca7c14
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-03 14:32:20 +0530

    Update code.cpp

--- 881/code.cpp
+++ 881/code.cpp
@@ -1,1 +1,37 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) 
+    {
+        sort(people.begin(),people.end());
 
+        int ct=0,i=0;
+        int j=people.size()-1;
+       while(i<=j)
+        {
+                if(people[j]==limit)
+                {
+                          ct++;
+                          j--;
+                }
+                else if(people[i]+people[j]<=limit)
+                {
+                          ct++;
+                          i++;
+                          j--;
+
+                }
+                else if(people[j]<=limit)
+                {
+                          ct++;
+                          j--;
+                }
+               
+                 //i++;
+                 //j--;
+           
+         }
+      
+        return ct;
+        
+    }
+};
