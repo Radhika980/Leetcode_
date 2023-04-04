07ef66bc164fe5628b75f84a938cb3d0028507d2
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-04 14:40:59 +0530

    Update code.cpp

--- 2006/code.cpp
+++ 2006/code.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    int countKDifference(vector<int>& nums, int k) 
+    {
+        int ct=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=i+1;j<nums.size();j++)
+            {
+                // cout<<"i="<<nums[i]<<" ";
+                // cout<<"j="<<nums[j]<<" ";
+                if(abs(nums[i]-nums[j])==k)ct++;
+            }
+        }
+        return ct;
+    }
+};
