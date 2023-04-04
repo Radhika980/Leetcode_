cad5ee9b6d84d42427e6e936cfd4e586e79691b8
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-04 18:35:51 +0530

    Update code.cpp

--- 2405/code.cpp
+++ 2405/code.cpp
@@ -1,1 +1,18 @@
 
+class Solution {
+public:
+    int partitionString(string s) 
+    {
+        
+        int ct=1;
+        unordered_map<char,bool>mp;
+        for(int i=0;i<s.size();i++)
+        {
+            if(mp[s[i]]==true)ct++,mp.clear();
+            mp[s[i]]=true;
+        }
+        return ct;
+
+    }
+};
+
