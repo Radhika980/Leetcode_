bf0c7d04a189f4a593b8489ab16d5154a153a0bb
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-05 23:17:00 +0530

    Update code.cpp

--- 2439/code.cpp
+++ 2439/code.cpp
@@ -1,1 +1,18 @@
+class Solution {
+public:
+    int minimizeArrayValue(vector<int>& nums)
+    {
 
+        long long ans=0,cur=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            cur+=nums[i];
+
+            long long val=ceil((cur)/(i+1.0));
+            //storing max in the ans
+            ans=max(ans,val);
+
+        }
+        return ans;
+    }
+};
