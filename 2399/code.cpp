83c35f84e86d33f3839d6b2416133eb2afa484d0
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-07 15:04:01 +0530

    Update code.cpp

--- 2399/code.cpp
+++ 2399/code.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
+    bool checkDistances(string s, vector<int>& distance) 
+    {
+        int aa=0,bb=0,cc=0;
+        for(int i=0;i<s.size()-1;i++)
+        {
+            aa=i;
+            for(int j=i+1;j<s.size();j++)
+            {
+                if(s[i]==s[j])
+                {
+                    bb=j;
+                    cc=bb-aa-1;
+                    if(cc!=distance[int(s[i])-int('a')])return false;
+                    //break;
+                }
 
+            }
+            
+            
+        }
+        return true;
+        
+    }
+    
+};
