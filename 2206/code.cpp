9cc4e0f0042592ed5bec2b30b54a1d2c7f4c3b00
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-07 11:38:51 +0530

    Update code.cpp

--- 2206/code.cpp
+++ 2206/code.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) 
+    {
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]++;
+        }
+        for(auto [data,fre]:mp)
+        {
+            if(fre%2!=0)return false;
+        }
+        return true;
+    }
+};
