54b161e2bb4965ba6df40f9e8c47c4f28bd90cae
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-07 18:50:57 +0530

    Update code.cpp

--- 728/code.cpp
+++ 728/code.cpp
@@ -1,1 +1,28 @@
 
+class Solution {
+public:
+bool check(int i)
+        {
+            int n=i;
+            int temp=i;
+             while(n>0)
+               {
+                   if(n%10==0)return false;
+                   if(temp%(n%10)!=0)return false;
+                   else n=n/10;
+               }
+               return true;
+        }
+    vector<int> selfDividingNumbers(int left, int right) 
+    {
+        vector<int>ans;
+        
+        
+       for(int i=left;i<=right;i++)
+       {
+           if(check(i)==true)ans.push_back(i);
+           
+       }
+        return ans;
+    }
+};
