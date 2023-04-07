9ac3ffb316d5945dc97f4b4d7d53e17c13780b80
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-07 18:24:43 +0530

    Update code.cpp

--- 2283/code.cpp
+++ 2283/code.cpp
@@ -1,1 +1,27 @@
-
+class Solution {
+public:
+    bool digitCount(string num) 
+    {
+        int n=num.size();
+        int flag=-1;
+        unordered_map<int,int>map;
+        
+        for(int i=0;i<num.size();i++)
+        {
+            map[num[i] -'0']++;
+        }
+        if(n==1)return false;
+        for(int i=0;i<num.size();i++)
+        {
+            //cout<<map[i]<<"\n";
+              if(num[i]-'0'!=map[i])
+              {
+                  return false;
+              }
+              //i++;
+        }
+        //if(flag==0) return false;
+        return true; 
+    }
+    
+};
