07c62b95813bc58935bbc228765e988e3ca3d63f
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-08 19:42:31 +0530

    Update code.cpp

--- 485/code.cpp
+++ 485/code.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) 
+    {
+        int sum=0,ans=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==1)
+            {
+                sum++;
+                ans=max(ans,sum);
+            }
+            else if(nums[i]==0)sum=0;
+        }
+       return ans; 
+    }
+};
