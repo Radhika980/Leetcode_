0e28f58597a0c38014c01c04eaca3cd71e4830e5
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-08 19:54:38 +0530

    Update code.cpp

--- 1343/code.cpp
+++ 1343/code.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr, int k, int threshold) 
+    {
+        int ct=0;
+        int maxsum=0;
+        if(arr.size()<k)return -1;
+        for(int i=0;i<k;i++)
+        {
+         maxsum+=arr[i];
+        }
+        if(floor(maxsum/k)>=threshold)ct++;
+        int window_sum=maxsum;
+        for(int i=k;i<arr.size();i++)
+        {
+            window_sum+=arr[i]-arr[i-k];
+            if(floor(window_sum/k)>=threshold)ct++;
+        }
+       
+        return ct;
+    }
+};
