a0c4d910deb62963566286807f21041019ae10a4
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-09 12:25:08 +0530

    Update code.cpp

--- 2068/code.cpp
+++ 2068/code.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    bool checkAlmostEquivalent(string word1, string word2) 
+    {
+        map<char,int>mp;
+        map<char,int>mp1;
+        for(char c:word1) mp[c]++;
+        for(char c:word2) mp1[c]++;
+        for(char c:word1)
+        {
+         if(abs(mp[c]-mp1[c])>3)return false;
+        }
+        for(char c:word2)
+        {
+        if(abs(mp[c]-mp1[c])>3)return false;
+        }
+        return true;
+     
+        
+    }
+};
