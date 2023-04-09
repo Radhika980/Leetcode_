5d67d1f9d5622163f911b8646bde8ec196d9c607
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-09 11:26:23 +0530

    Update code.cpp

--- 888/code.cpp
+++ 888/code.cpp
@@ -1,1 +1,34 @@
+class Solution {
+public:
+    vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) 
+    {
+        vector<int>ans;
+        int sum=0,s=0,totsum=0;
+        for(int i=0;i<aliceSizes.size();i++)
+        {
+            sum+=aliceSizes[i];
+        }
+        for(int i=0;i<bobSizes.size();i++)
+        {
+            s+=bobSizes[i];
+        }
+        
+            totsum=(sum+s)/2;
+        
+        for(int i=0;i<aliceSizes.size();i++)
+         {
+             for(int j=0;j<bobSizes.size();j++)
+             {
+                 if(sum-aliceSizes[i]+bobSizes[j]==totsum)
+                 {
+                     ans.push_back(aliceSizes[i]);
+                     ans.push_back(bobSizes[j]);
+                     return ans;
+                 }
+             }
+        }
+        return ans;
+        
+    }
+};
 
