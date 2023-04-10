b7bc1768244f74932b02b15e228e2352e6d03c5d
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-10 18:48:53 +0530

    Update code.cpp

--- 20/code.cpp
+++ 20/code.cpp
@@ -1,1 +1,25 @@
-
+class Solution {
+public:
+    bool isValid(string s) 
+    {
+        stack<char>st;
+        
+        for(int i=0;i<s.size();i++)
+        {  
+            if(st.empty())st.push(s[i]);
+            else if(s[i]=='(')st.push(s[i]);
+            else if(s[i]=='{')st.push(s[i]);
+            else if(s[i]=='[')st.push(s[i]);
+            else if(s[i]==')' && st.top()=='(')st.pop();
+            else if(s[i]=='}' && st.top()=='{')st.pop();
+            else if(s[i]==']' && st.top()=='[')st.pop();
+             
+        }
+        if(st.empty())
+        return true;
+        else
+        return false;
+        
+        
+    }
+};
