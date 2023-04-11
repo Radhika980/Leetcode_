64f298a0ef34fb96022978cd614e6e74abe2f926
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-11 13:00:07 +0530

    Update code.cpp

--- 2085/code.cpp
+++ 2085/code.cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+public:
+    int countWords(vector<string>& words1, vector<string>& words2)
+    {
+        unordered_map<string,int>mp;
+        unordered_map<string,int>mp1;
+        int count=0;
+        for(auto c:words1)
+        {
+            mp[c]++;
+        }
+        for(auto ct:words2)
+        {
+            mp1[ct]++;
+        }
+        for(auto ch :words1)
+        {
+            if(mp[ch]==1 && mp1[ch]==1 && mp[ch]==mp1[ch])
+            count++;
+        }
+        return count;
+    }
+};
