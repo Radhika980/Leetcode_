c2bdf54c330fa6fc4d09188d7d6d7b28bb9b8016
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-11 11:43:25 +0530

    Update code2.cpp

--- 2390/code2.cpp
+++ 2390/code2.cpp
@@ -1,1 +1,19 @@
-
+class Solution {
+public:
+    string removeStars(string s) 
+    {
+        string k="";
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='*')
+            {
+                k.pop_back();
+            }
+            else
+            // k.push_back(s[i]);
+            k+=s[i];
+            //k=k+s[i];
+        }
+        return k;
+    }
+};
