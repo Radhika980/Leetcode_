d1b2dd90d0e29068cc4256f26353797099220b6a
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-11 11:57:56 +0530

    Create code2.cpp

---
+++ 1047/code2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string removeDuplicates(string s)
+    {
+        string k="";
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]==k.back())
+            {
+                k.pop_back();
+                //k.pop_back();
+            }
+            else
+            // k.push_back(s[i]);
+            k+=s[i];
+            
+        }
+        return k;
+         }
+};
