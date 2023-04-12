8f16dc019d1975b468f86d04bed3f3c67e1ab495
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-12 09:24:17 +0530

    Update code.cpp

--- 1598/code.cpp
+++ 1598/code.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+public:
+    int minOperations(vector<string>& logs) 
+    {
+      
+        int ct=0;
+        for(int i=0;i<logs.size();i++)
+        {
+            if(logs[i]!="./" && logs[i]!="../")ct++;
+            else if(logs[i]=="./")continue;
+            else if(logs[i]=="../")
+            {
+                if(ct==0)continue;
+                else ct--;
+            }
+        }
+        if(ct>0)
+        return ct;
+        else return 0;
+        
+    }
+};
