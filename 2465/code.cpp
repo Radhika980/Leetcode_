f5411aad7c1d697d10191b373536da8f25216be3
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-13 16:05:43 +0530

    Update code.cpp

--- 2465/code.cpp
+++ 2465/code.cpp
@@ -1,1 +1,21 @@
-
+class Solution {
+public:
+    int distinctAverages(vector<int>& nums) 
+    {
+        set<float>ans;
+        sort(nums.begin(),nums.end());
+        int l=0;
+        int r=nums.size()-1;
+        while(l<=r)
+        {
+        float a=(nums[l]+nums[r])/(float)2;
+        ans.insert(a);
+        l++;
+        r--;
+        }
+        //cout<<ans;
+        return ans.size();
+        
+        
+    }
+};
