7d2e3859f092ad943392b64fb23bbea57c049466
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-13 14:16:55 +0530

    Update code.cpp

--- 946/code.cpp
+++ 946/code.cpp
@@ -1,1 +1,43 @@
 
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+       stack<int>st;
+       int j=0,i=0;  
+       if(pushed==popped)return true;
+       
+       while(i<pushed.size())
+       {
+           if(st.empty()==true)
+           {
+               st.push(pushed[i]);
+               i++;
+           }
+           else if(st.top()!=popped[j])
+           {
+                st.push(pushed[i]);
+                i++;
+           }
+            else if(j<popped.size() && st.top()==popped[j])
+           {
+                    st.pop();
+                    j++;
+            }
+          
+       }
+    
+    while(j<popped.size())
+    {
+        if(st.top()==popped[j] )
+           {
+               st.pop();
+               j++;
+           }
+           else return false;
+    }
+        if(st.empty()==true)
+      return true;
+      else return false;
+      
+    }
+};
