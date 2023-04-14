e9b7335a7e7b0277a7ffc8e991157a7210836b57
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-14 22:32:32 +0530

    Update code.cpp

--- 1920/code.cpp
+++ 1920/code.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums)
+    {
+        int a=nums.size();
+        vector<int>ans;
+        for(int i=0;i<a;i++)
+        {
+            ans.push_back(nums[nums[i]]);
+        }
+       return ans; 
+    }
+};
