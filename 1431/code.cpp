7b59415d3530c09483408c66982e089c88387cd1
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-17 08:05:44 +0530

    Update code.cpp

--- 1431/code.cpp
+++ 1431/code.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies)
+    {
+        vector<bool>ans;
+        int m=*max_element(candies.begin(),candies.end());
+        for(int i=0;i<candies.size();i++)
+        {
+            cout<<candies[i]+extraCandies<<"\n";
+            if((candies[i]+extraCandies)>=m)
+            ans.push_back(true);
+            else
+            ans.push_back(false);
+        }
+       return ans; 
+    }
+};
