bf3487572304b372e9a22d392e4d5e021ea729d3
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-18 08:36:28 +0530

    Update code.cpp

--- 1768/code.cpp
+++ 1768/code.cpp
@@ -1,1 +1,27 @@
-
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2)
+    {
+        string ans="";
+        int n=word1.length();
+        int m=word2.length();
+        int j=min(n,m);
+       for(int i=0;i<j;i++)
+        {
+        ans+=word1[i];
+        ans+=word2[i];
+       }
+       for(int i=j;i<n;i++)
+        {
+        ans+=word1[i];
+       
+       }
+       for(int i=j;i<m;i++)
+        {
+      
+        ans+=word2[i];
+       }
+        return ans;
+        
+    }
+};
