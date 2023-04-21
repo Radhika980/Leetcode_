3b02b183b6641683e386127dfa764df5c9962f2d
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-21 23:04:47 +0530

    Update code.cpp

--- 2215/code.cpp
+++ 2215/code.cpp
@@ -1,1 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2)
+    {
+        vector<vector<int>>ans;
+        unordered_map<int,int>mp1;
+        unordered_map<int,int>mp2;
+        vector<int>ans1;
+        vector<int>ans2;
+        for(int i=0;i<nums1.size();i++)
+        {
+            mp1[nums1[i]]++;
+        }
+        for(int i=0;i<nums2.size();i++)
+        {
+            mp2[nums2[i]]++;
+        }
+        for(auto [data,freq]:mp1)
+        {
+            
+                if(mp2[data]>=1)continue;
+                else ans1.push_back(data);
+           
+            
+        }
+        for(auto [data,freq]:mp2)
+        {
+            
+                if(mp1[data]>=1)continue;
+                else ans2.push_back(data);
+            
+            
+        }
+        ans.push_back(ans1);
+        ans.push_back(ans2);
+        return ans;
 
+        
+    }
+};
