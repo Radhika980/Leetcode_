bdc6681fd7e95a30d014314fcb31c02bf2baacc1
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-22 20:07:57 +0530

    Update code.cpp

--- 2540/code.cpp
+++ 2540/code.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) 
+    {
+        int l=0;
+        int r=0;
+        
+        while(l<nums1.size() && r<nums2.size())
+        {
+            if(nums1[l]==nums2[r])return nums1[l];
+            else if(nums1[l]>nums2[r])r++;
+            else if(nums1[l]<nums2[r])l++;
+        }
+        return -1;
+        
+    }
+};
