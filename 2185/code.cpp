d14914341fb2d6617cbc037e0b0f39b0258b8855
Radhika <73791285+Radhika980@users.noreply.github.com>
2023-04-28 19:20:24 +0530

    Create code.cpp

---
+++ 2185/code.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) 
+    {
+        int ct=0;
+        for(int i=0;i<words.size();i++)
+        {
+            if(words[i].substr(0,pref.size())==pref)
+            {
+                ct++;
+            }
+        }
+      return ct;  
+    }
+};
